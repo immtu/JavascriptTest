8056ba3abc8260e7fbc3cb6b05514ae0da1d0c6f
immtu <imtiyaz.i.khan@hotmail.com>
2022-04-18 16:48:04 +0530

    QA issue resolved, Added phone number functionality.

--- fe-code/js/app.js
+++ fe-code/js/app.js
@@ -1,71 +1,131 @@
 $(document).ready(function () {
+    const txtSearchfor = $('input[name="txtSearch"]');
 
-  $("#btn-search").on("click", function (e) {
-    e.preventDefault();
-    localStorage.clear(); //Clears storage for next request
-    email = $('input[type="text"]').val().toLowerCase();
+    $('input[name="search"]').on("change", function (e) {
+        e.preventDefault();
+        const invalidEmailError = 'Please enter a valid email address';
+        const invalidPhoneError = 'Please enter a valid phone number';
+        const emailPlaceHolder = 'Enter an Email Address';
+        const phonePlaceHolder = 'Enter a Phone Number';
+        let searchType = $('input[name="search"]:checked').val();
+        showHideError(false);
+        if (searchType.toLowerCase() === "emailaddress") {
+            $('.error-msg').html(invalidEmailError);
+            txtSearchfor[0].placeholder = emailPlaceHolder;
+        }
+        else {
+            $('.error-msg').html(invalidPhoneError);
+            txtSearchfor[0].placeholder = phonePlaceHolder;
+        }
+    });
 
-    var x, y;
-    regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    if (email.match(regEx)) {
-      x = true;
-    } else {
-      x = false;
+
+    $("#btn-search").on("click", function (e) {
+        e.preventDefault();
+        let searchType = $('input[name="search"]:checked').val();
+        if (searchType.toLowerCase() === "emailaddress") {
+            processSearch(true);
+        }
+        else {
+            processSearch(false);
+        }
+        
+    });
+
+    
+    $('input[type="text"]').keypress(function (event) {
+        let searchValue, url;
+        let searchType = $('input[name="search"]:checked').val();
+        searchValue = txtSearchfor.val();
+
+        var isvalidated = false;
+        if (searchType.toLowerCase() === "emailaddress") {
+             if (validation.isEmailAddress(searchValue)) {
+                url = 'https://ltv-data-api.herokuapp.com/api/v1/records.json?email=' + searchValue;
+                isvalidated = true;
+            }
+        }
+        else {
+            if (validation.isPhoneNumber(searchValue)) {
+                url = 'https://ltv-data-api.herokuapp.com/api/v1/records.json?phone=' + searchValue;
+                isvalidated = true;
+            }
+        }
+
+        keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode == '13') {
+            /**
+             * Makes a request to ltv API to search an specific email address.
+             * If there's a response, it gets stored in the local storage and redirects to results page
+             */
+            event.preventDefault();
+            localStorage.clear(); //Clears storage for next request
+            showHideError(false);
+            if (isvalidated === true) {
+                getRespectiveInormation(url);
+
+            } else if (isvalidated !== true) {
+                showHideError(true);
+            }
+        }
+    });
+
+    function processSearch(isEmail) {
+        localStorage.clear(); //Clears storage for next request.
+        let searchValue = txtSearchfor.val();
+        var isvalidated = false;
+        let url;
+        if (isEmail) {
+             if (validation.isEmailAddress(searchValue.toLowerCase())) {
+                url = 'https://ltv-data-api.herokuapp.com/api/v1/records.json?email=' + searchValue;
+                isvalidated = true;
+            }
+        }
+        else {
+            if (validation.isPhoneNumber(searchValue)) {
+                url = 'https://ltv-data-api.herokuapp.com/api/v1/records.json?phone=' + searchValue;
+                isvalidated = true;
+            }
+        }
+
+        if (isvalidated === true) {
+            showHideError(false);
+            getRespectiveInormation(url);
+        } else if (isvalidated !== true) {
+            showHideError(true);
+        }
     }
 
-    if (x === true) {
-      document.querySelector('input[type="text"]').parentNode.classList.remove("error");
-      const proxyurl = "";
-      const url =
-        'https://ltv-data-api.herokuapp.com/api/v1/records.json?email=' + email;
-      fetch(proxyurl + url)
-        .then((response) => response.text())
-        .then(function (contents) {
-          localStorage.setItem("userObject", contents);
-          window.location.href = "result.html";
-        })
-        .catch((e) => console.log(e));
-    } else if (x !== true) {
-      document.querySelector('input[type="text"]').parentNode.classList.add("error");
+    function showHideError(iserror) {
+        if (iserror) {
+            txtSearchfor[0].parentNode.classList.add("error");
+        }
+        else {
+            txtSearchfor[0].parentNode.classList.remove("error");
+        }
     }
-  });
 
-  $('input[type="text"]').keypress(function (event) {
-    email = $('input[type="text"]').val().toLowerCase();
-    regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    if (email.match(regEx)) {
-      x = true;
-      document.querySelector('input[type="text"]').parentNode.classList.remove("error");
-    } else {
-      x = false;
-    }
-    keycode = (event.keyCode ? event.keyCode : event.which);
-    if (keycode == '13') {
-      /**
-       * Makes a request to ltv API to search an specific email address.
-       * If there's a response, it gets stored in the local storage and redirects to results page
-       */
-      event.preventDefault();
-      localStorage.clear(); //Clears storage for next request
-
-      var x, y;
-
-
-      if (x === true) {
+    function getRespectiveInormation(url) {
         const proxyurl = "";
-        const url =
-          'https://ltv-data-api.herokuapp.com/api/v1/records.json?email=' + email;
+        $("body").addClass("loading");
         fetch(proxyurl + url)
-          .then((response) => response.text())
-          .then(function (contents) {
-            localStorage.setItem("userObject", contents);
-            window.location.href = "result.html";
-          })
-          .catch((e) => console.log(e));
-      } else if (x !== true) {
-        document.querySelector('input[type="text"]').parentNode.classList.add("error");
-      }
+            .then((response) => response.text())
+            .then(function (contents) {
+                localStorage.setItem("userObject", contents);
+                window.location.href = "result.html";
+            })
+            .catch((e) => console.log(e));
     }
-  });
+
+    let validation = {
+        isEmailAddress: function (email) {
+            const emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+            return email.match(emailRegEx);  // returns a boolean
+        },
+        isPhoneNumber: function (phone) {
+            const phoneRegEx = /^[0-9]{10}$/;
+            return phone.match(phoneRegEx);  // returns a boolean
+        }
+    };
 
 });
